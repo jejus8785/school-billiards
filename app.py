d44f735d934c4878777ee84ee4b89925637924d2
jejus8785 <jejus0002@gmail.com>
2026-08-30 16:16:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ except ImportError:
     psycopg = None
     dict_row = None
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.', static_folder='.')
 app.secret_key = os.environ.get("SECRET_KEY", "local-development-secret-key")
 ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "ddyy1016")
 MAX_TEAMS = 2
